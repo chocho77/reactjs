55ce0a0493e07155fcb1c1b8b82f619b821094e0
chocho77 <chavmom@gmail.com>
2022-09-11 04:52:14 +0300

    update h2 tag in div tag

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 
 function App() {
   const name = "Chavdar";
+  const x = true;
+
   return (
     <div className="App">
      <h1>Hello From React</h1>
-     <h2>Hello {name}</h2>
+     <h2>Hello {name} {x ? 'Yes' : 'No'}</h2>
     </div>
   );
 }
