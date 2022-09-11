43fb396360419d7e82d80e89917e05ef1bb81653
chocho77 <chavmom@gmail.com>
2022-09-11 05:30:59 +0300

    edit App.js Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 
 function App() {
-  const name = "Chavdar";
-  const x = true;
-
+ 
   return (
     <div className="App">
      <h1>Hello From React</h1>
-     <h2>Hello {name} {x ? 'Yes' : 'No'}</h2>
+     
     </div>
   );
 }
