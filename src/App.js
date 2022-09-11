ce999526df91007e3bffc65d95fc64020c73eb82
chocho77 <chavmom@gmail.com>
2022-09-11 04:49:22 +0300

    add another tag h2 with message Hello ... name

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
 function App() {
+  const name = "Chavdar";
   return (
     <div className="App">
      <h1>Hello From React</h1>
+     <h2>Hello {name}</h2>
     </div>
   );
 }
